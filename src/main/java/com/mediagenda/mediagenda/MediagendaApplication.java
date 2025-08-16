84b9575ff9765baeb186c59b6275ce0a3b160b29
dayana-vega <da.vegab@duocuc.cl>
2025-08-16 16:14:34 -0400

    Primer commit de MediAgenda

---
+++ src/main/java/com/mediagenda/mediagenda/MediagendaApplication.java
@@ -0,0 +1,13 @@
+package com.mediagenda.mediagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediagendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediagendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediagenda/mediagenda/MediagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediagenda.mediagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediagendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
