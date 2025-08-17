347471bf1eabe13038487b1c0d8817af26225f29
dayana-vega <da.vegab@duocuc.cl>
2025-08-17 19:24:30 -0400

    se agregan anotaciones service y autowired a citaRepository

--- src/main/java/com/mediagenda/mediagenda/cita/service/citaService.java
+++ src/main/java/com/mediagenda/mediagenda/cita/service/citaService.java
@@ -1,5 +1,18 @@
 package com.mediagenda.mediagenda.cita.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mediagenda.mediagenda.cita.repository.CitaRepository;
+import com.mediagenda.mediagenda.usuario.repository.UsuarioRepository;
+
+@Service
 public class citaService {
+
+    @Autowired
+    private CitaRepository citaRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
 }
