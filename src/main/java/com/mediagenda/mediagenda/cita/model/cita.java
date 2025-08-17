7dccfcf17ed71d3e7050d58369a79a63e9369a14
dayana-vega <da.vegab@duocuc.cl>
2025-08-17 17:06:14 -0400

    LLENADO DE CLASE CITA

--- src/main/java/com/mediagenda/mediagenda/cita/model/cita.java
+++ src/main/java/com/mediagenda/mediagenda/cita/model/cita.java
@@ -1,5 +1,61 @@
 package com.mediagenda.mediagenda.cita.model;
 
-public class cita {
+import java.time.LocalDateTime;
 
+import com.mediagenda.mediagenda.enums.EstadoCita;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+
+@Entity
+public class cita {
+    @Id
+    private int id;
+    private LocalDateTime fechaCita;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private EstadoCita estadoCita;
+
+    public cita() {
+    }
+
+    public cita(int id, LocalDateTime fechaCita, @NotNull EstadoCita estadoCita) {
+        this.id = id;
+        this.fechaCita = fechaCita;
+        this.estadoCita = estadoCita;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFechaCita() {
+        return fechaCita;
+    }
+
+    public void setFechaCita(LocalDateTime fechaCita) {
+        this.fechaCita = fechaCita;
+    }
+
+    public EstadoCita getEstadoCita() {
+        return estadoCita;
+    }
+
+    public void setEstadoCita(EstadoCita estadoCita) {
+        this.estadoCita = estadoCita;
+    }
+
+    
+
+
+    
 }
