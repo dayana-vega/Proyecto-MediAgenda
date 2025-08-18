0a92bbb1e0492cde5e23abd333e7bfd0b40b5a6c
dayana.vega <da.vegab@duocuc.cl>
2025-08-18 10:17:25 -0400

    Se crea CrearCita en citaService

--- src/main/java/com/mediagenda/mediagenda/cita/service/citaService.java
+++ src/main/java/com/mediagenda/mediagenda/cita/service/citaService.java
@@ -3,16 +3,22 @@ package com.mediagenda.mediagenda.cita.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mediagenda.mediagenda.cita.model.Cita;
 import com.mediagenda.mediagenda.cita.repository.CitaRepository;
 import com.mediagenda.mediagenda.usuario.repository.UsuarioRepository;
 
 @Service
-public class CitaService {
+public class citaService {
 
     @Autowired
     private CitaRepository citaRepository;
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    public String crearCita(Cita cita){
+        Optional<Cita> citaExistente = citaRepository.findByPacieteRut(cita.getPaciente).getRut());
+        return;
+    }
 
 }
