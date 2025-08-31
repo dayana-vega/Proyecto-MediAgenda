3d4982044a043d75a31b3dc0019d48aa574ca47b
dayana-vega <da.vegab@duocuc.cl>
2025-08-31 16:17:15 -0400

    llenado de atributos, getters and setters  clase horario

--- src/main/java/com/mediagenda/mediagenda/horario/model/HorarioMedico.java
+++ src/main/java/com/mediagenda/mediagenda/horario/model/HorarioMedico.java
@@ -1,5 +1,73 @@
 package com.mediagenda.mediagenda.horario.model;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+import com.mediagenda.mediagenda.usuario.model.Medico;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class HorarioMedico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private DayOfWeek dia;
+    private LocalTime horarioInicio;
+    private LocalTime horarioFin;
+
+    @ManyToOne
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public DayOfWeek getDia() {
+        return dia;
+    }
+
+    public void setDia(DayOfWeek dia) {
+        this.dia = dia;
+    }
+
+    public LocalTime getHorarioInicio() {
+        return horarioInicio;
+    }
+
+    public void setHorarioInicio(LocalTime horarioInicio) {
+        this.horarioInicio = horarioInicio;
+    }
+
+    public LocalTime getHorarioFin() {
+        return horarioFin;
+    }
+
+    public void setHorarioFin(LocalTime horarioFin) {
+        this.horarioFin = horarioFin;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    
+
+
 
 }
